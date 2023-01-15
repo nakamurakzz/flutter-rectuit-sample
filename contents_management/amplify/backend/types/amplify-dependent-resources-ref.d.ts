da7e0406ee940d462e04bf8be23abae511f6511e
nakamurakzz <kazutaka.nakamura@ncdc.co.jp>
2023-01-15 16:00:24 +0900

    amplify add api

--- contents_management/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ contents_management/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "apiee6389db": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    }
+  },
   "auth": {
     "contentsmanagement8f900994": {
       "AppClientID": "string",
@@ -8,6 +15,27 @@ export type AmplifyDependentResourcesAttributes = {
       "UserPoolArn": "string",
       "UserPoolId": "string",
       "UserPoolName": "string"
+    }
+  },
+  "function": {
+    "contentsmanagemente92b20e7": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  },
+  "storage": {
+    "dynamo9bbd7ebf": {
+      "Arn": "string",
+      "Name": "string",
+      "PartitionKeyName": "string",
+      "PartitionKeyType": "string",
+      "Region": "string",
+      "SortKeyName": "string",
+      "SortKeyType": "string",
+      "StreamArn": "string"
     }
   }
 }
